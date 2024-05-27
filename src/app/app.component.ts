fd6c270a58b370670267b238f293fbff6ce7686c
Loosli Ben <Ben.Loosli@bystronic.com>
2024-05-27 11:32:10 +0200

    first content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import * as bootstrap from 'bootstrap';
+
 import { RouterOutlet } from '@angular/router';
+import { bootstrapApplication } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +11,15 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'stw-gibb-nid-statisch';
+  @ViewChild('helloModal') helloEl?: ElementRef;
+  modal?: bootstrap.Modal;
+  ngAfterViewInit() {
+    this.modal = new bootstrap.Modal(this.helloEl?.nativeElement, {});
+  }
+
+  trigger() {
+    this.modal?.toggle();
+  }
 }
